8e1c97e101f341396c9bfada01b372e6b1e41890
Majeet-MnB <manjeetbilthare123@gmail.com>
2023-07-31 13:35:14 +0530

    main file and validation

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import sys, os
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
-'''from Insurance.components.data_validation import DataValidation
-from Insurance.components.data_transformation import DataTransformation
-from Insurance.components.model_trainer import ModelTrainer
-from Insurance.components.model_evaluation import ModelEvaluation
-from Insurance.components.model_pusher import ModelPusher'''
+from Insurance.components.data_validation import DataValidation
+#from Insurance.components.data_transformation import DataTransformation
+#from Insurance.components.model_trainer import ModelTrainer
+#from Insurance.components.model_evaluation import ModelEvaluation
+#from Insurance.components.model_pusher import ModelPusher
 
 '''def test_logger_and_expection():
     try:
@@ -20,7 +20,7 @@ from Insurance.components.model_pusher import ModelPusher'''
     except Exception as e:
         logging.debug(str(e))
         raise InsuranceException(e, sys)'''
-    
+
 
 if __name__ == "__main__":
     try:
@@ -28,6 +28,11 @@ if __name__ == "__main__":
         data_ingestion_conig = config_entity.DataIngestionConfig(traning_pipeline_config = traning_pipeline_config)
         print(data_ingestion_conig.to_dict())      
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_conig)
-        data_ingestion_artifact = data_ingestion.initiate_data_ingestion() 
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=traning_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
+        #data_validation = DataValidation(data_validation_config=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
+        data_validation_artifact = data_validation.initiate_data_validation()
     except Exception as e:
         print(e)    
